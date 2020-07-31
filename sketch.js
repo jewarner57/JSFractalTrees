6ed3fab1067ec4334d57c0754a17dbe728067360
Jonathan Warner <jewarner57@gmail.com>
2020-07-31 18:15:15 -0400

    feat: add slider to control tree growth

--- sketch.js
+++ sketch.js
@@ -1,30 +1,35 @@
 
+let branchLength = 100;
 
 function setup() {
-  createCanvas(400, 800);
+  createCanvas(400, 400);
+
+  slider = createSlider(1, branchLength, 1);
+  slider.position(10, 10);
+  slider.style('width', '100px');
 }
 
 function draw() {
   background(100);
-  var len = 100;
+
   translate(200, height)
   stroke(255);
-  branch(100);
 
+  branch(branchLength);
 }
 
 function branch(len) {
-  
-  line(0, 0, 0,-len);
+
+  line(0, 0, 0, -len);
   translate(0, -len);
-  if(len > 2) {
+  if (len > slider.value()) {
     push();
     rotate(TWO_PI / 0.92);
-    branch(len*0.67)
+    branch(len * 0.67)
     pop();
     push();
     rotate(-TWO_PI / 0.92);
-    branch(len*0.67)
+    branch(len * 0.67)
     pop();
   }
 }
